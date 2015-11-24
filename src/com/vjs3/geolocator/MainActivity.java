524d4830914b495e8825b7febacc1e726b5172fd
Vishwajeet Srivastava <vishwajeets912@gmail.com>
2015-11-25 04:06:07 +0530

    first commit

---
+++ src/com/vjs3/geolocator/MainActivity.java
@@ -0,0 +1,102 @@
+package com.vjs3.geolocator;
+
+import com.vjs3.geolocator.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends Activity {
+
+	// Google Map
+	private GoogleMap googleMap;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		try {
+			// Loading map
+			initilizeMap();
+
+			// Changing map type
+			googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+			// googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+			// googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+			// googleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+			// googleMap.setMapType(GoogleMap.MAP_TYPE_NONE);
+
+			// Showing / hiding your current location
+			googleMap.setMyLocationEnabled(true);
+
+			// Enable / Disable zooming controls
+			googleMap.getUiSettings().setZoomControlsEnabled(false);
+
+			// Enable / Disable my location button
+			googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+
+			// Enable / Disable Compass icon
+			googleMap.getUiSettings().setCompassEnabled(true);
+
+			// Enable / Disable Rotate gesture
+			googleMap.getUiSettings().setRotateGesturesEnabled(true);
+
+			// Enable / Disable zooming functionality
+			googleMap.getUiSettings().setZoomGesturesEnabled(true);
+
+			double latitude = 26.935588;
+			double longitude = 75.921596;
+			MarkerOptions marker = new MarkerOptions().position(
+					new LatLng(latitude, longitude)).title("Hello Maps ");
+
+			marker.icon(BitmapDescriptorFactory
+					.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+			googleMap.addMarker(marker);
+
+			CameraPosition cameraPosition = new CameraPosition.Builder()
+					.target(new LatLng(latitude, longitude)).zoom(15).build();
+
+			googleMap.animateCamera(CameraUpdateFactory
+					.newCameraPosition(cameraPosition));
+			// LatLng position = new LatLng(latitude, longitude);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		initilizeMap();
+	}
+
+	/**
+	 * function to load map If map is not created it will create it for you
+	 * */
+	private void initilizeMap() {
+		if (googleMap == null) {
+			googleMap = ((MapFragment) getFragmentManager().findFragmentById(
+					R.id.map)).getMap();
+
+			// check if map is created successfully or not
+			if (googleMap == null) {
+				Toast.makeText(getApplicationContext(),
+						"Sorry! unable to create maps", Toast.LENGTH_SHORT)
+						.show();
+			}
+		}
+	}
+
+}
